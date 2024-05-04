426fa2c5c528c48b6cdf501408fed2fb0c2906b3
davi holanda <daviht7@gmail.com>
2024-05-04 11:35:08 -0300

    pacote config

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/router"
 	"fmt"
 	"log"
@@ -9,10 +10,12 @@ import (
 
 func main() {
 
-	fmt.Println("Rodando API")
+	config.Carregar()
+
+	fmt.Printf("Rodando API na PORTA %d...", config.Porta)
 
 	r := router.Gerar()
 
-	log.Fatal(http.ListenAndServe(":5000", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 
 }
