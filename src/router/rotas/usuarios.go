a98ebeab97790d858788144f4313392ed4872631
davi holanda <daviht7@gmail.com>
2024-05-30 16:54:25 -0300

    parar de seguir usuario

--- src/router/rotas/usuarios.go
+++ src/router/rotas/usuarios.go
@@ -43,4 +43,10 @@ var rotasUsuarios = []Rota{
 		Funcao:             controller.SeguirUsuario,
 		RequerAutenticacao: true,
 	},
+	{
+		URI:                "/usuarios/{usuarioId}/parar-de-seguir",
+		Metodo:             http.MethodPost,
+		Funcao:             controller.ParaDeSeguirUsuario,
+		RequerAutenticacao: true,
+	},
 }
