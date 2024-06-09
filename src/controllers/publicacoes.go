0a302e7a825841831246e52cece9e640c280e99d
davi holanda <daviht7@gmail.com>
2024-06-09 20:50:39 -0300

    Rotas de publicações

---
+++ src/controllers/publicacoes.go
@@ -0,0 +1,28 @@
+package controllers
+
+import "net/http"
+
+//CriarPublicacao adiciona publicacacao no banco de dados
+func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//BuscarPublicacoes busca publicacoes no banco de dados
+func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//BuscarPublicacao busca uma publicacao no banco de dadso
+func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//AtualizarPublicacao atualiza publicacao no banco de dados
+func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//Deletarpublicacao deleta dados de uma publicacao
+func DeletarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
